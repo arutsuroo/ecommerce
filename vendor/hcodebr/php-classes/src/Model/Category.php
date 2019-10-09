7f37082bd1a10e0289373b1079f9c712601ec312
unknown <you@example.com>
2019-10-09 19:23:23 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -24,6 +24,8 @@ class Category extends Model{
         ));
         
         $this->setData($result[0]);
+        
+        Category::updateFile();
     }
     
     public function get($idcategory)
@@ -46,6 +48,20 @@ class Category extends Model{
             ':idcategory'=>$this->getidcategory()
         ]);
         
+        Category::updateFile();
+    }
+    
+    public function updateFile()
+    {
+        $categories = Category::listAll();
+        
+        $html = [];
+        
+        foreach ($categories as $row){
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+        
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
     }
 }
 
