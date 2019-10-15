00fa1fbfa8185d77b0e83ac016682d0d96ea959a
unknown <you@example.com>
2019-10-15 15:32:41 -0300

    Site Cadastro

--- site.php
+++ site.php
@@ -150,12 +150,14 @@ $app->get("/checkout", function (){
     ]);
 });
 
-$app->get("/login", function (){
+$app->get("/login", function(){
     
     $page = new Page();
     
     $page->setTpl("login", [
-        'error'=>User::getError()
+        'error'=>User::getError(),
+        'errorRegister'=>User::getErrorRegister(),
+        'registerValues'=>(isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name'=>'', 'email'=>'', 'phone']
     ]);
 });
 
@@ -180,6 +182,63 @@ $app->get("/logout", function(){
     User::logout();
     
     header("Location: /login");
+    exit;
+});
+
+$app->post("/register", function(){
+    
+    $_SESSION['registerValues'] = $_POST;
+    
+    if (!isset($_POST['name']) && $_POST['name'] == ''){
+        
+        User::setErrorRegister("Preencha o seu nome.");
+        
+        header('Location: /login');
+        exit;
+    }
+   
+    if (!isset($_POST['email']) && $_POST['email'] == ''){
+        
+        User::setErrorRegister("Preencha o seu email.");
+        
+        header('Location: /login');
+        exit;
+    }
+    
+    if (!isset($_POST['password']) && $_POST['password'] == ''){
+        
+        User::setErrorRegister("Preencha a sua senha.");
+        
+        header('Location: /login');
+        exit;
+    }
+    
+    if (User::checkLoginExist($_POST['email']) === true){
+        
+        User::setErrorRegister("Este endereço de email já está sendo usado por outro usuário.");
+        
+        header('Location: /login');
+        exit;
+        
+    }
+    
+    
+    $user = new User();
+    
+    $user->setData([
+       'inadmin'=>0,
+        'deslogin'=>$_POST['email'],
+        'desperson'=>$_POST['name'],
+        'desemail'=>$_POST['email'],
+        'despassword'=>$_POST['password'],
+        'nrphone'=>$_POST['phone']
+    ]);
+    
+    $user->save();
+    
+    User::login($_POST['email'], 'despassword'=>$_POST['password']);
+
+    header('Location: /checkout');
     exit;
 });
 
